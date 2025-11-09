0068cf0fb6990e199be700f7f0f5765f1de346e5
unknown <deekayedm@gmail.com>
2025-11-09 18:00:15 +0530

    taskcontroller completed

--- backend/controller/taskController.js
+++ backend/controller/taskController.js
@@ -290,8 +290,65 @@ const getDashboardData = async (req, res) => {
     }
 }
 
-const getUserDashboardData = async () => {
+const getUserDashboardData = async (req, res) => {
+    try {
+        const userId = req.user._id;
 
+        const totalTasks = await Task.countDocuments({ assignedTo: userId });
+        const pendingTasks = await Task.countDocuments({ assignedTo: userId, status: "Pending" })
+        const completedTasks = await Task.countDocuments({ assignedTo: userId, status: "Completed" });
+        const overdueTasks = await Task.countDocuments({
+            assignedTo: userId,
+            status: { $ne: "Completed" },
+            dueDate: { $lt: new Date() }
+        })
+
+        const taskStatuses = ["Pending", "In Progress", "Completed"]
+        const taskDistributionRaw = await Task.aggregate([
+            { $match: { assignedTo: userId } },
+            { $group: { _id: "$status", count: { $sum: 1 } } }
+        ]);
+
+        const taskDistribution = taskStatuses.reduce((acc, status) => {
+            const formattedKey = status.replace(/\s+/g, "");
+            acc[formattedKey] = taskDistributionRaw.find((item) => item._id === status)?.count || 0;
+            return acc;
+        }, {});
+
+        taskDistribution["All"] = totalTasks;
+
+        // Task distribution by priority
+        const taskPriorities = ["Low", "Medium", "HIgh"];
+        const taskPriorityLevelRaw = await Task.aggregate([
+            { $match: { assignedTo: userId } },
+            { $group: { _id: "$priority", count: { $sum: 1 } } }
+        ]);
+
+        const taskPrioritiesLevels = taskPriorities.reduce((acc, priority) => {
+            acc[priority] = taskPriorityLevelRaw.find((item) => item._id === priority)?.count || 0;
+            return acc;
+        }, {})
+
+        const recentTasks = await Task.find({ assignedTo: userId })
+            .sort({ createdAt: -1 })
+            .limit(10)
+            .select('title status priority duedate createdAt');
+
+        res.status(200).json({
+            statistics: {
+                totalTasks,
+                pendingTasks,
+                completedTasks,
+                overdueTasks
+            }, charts: {
+                taskDistribution,
+                taskPrioritiesLevels
+            },
+            recentTasks
+        })
+    } catch (error) {
+
+    }
 }
 
 
