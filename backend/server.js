0196092726ddc7b932d832994550852b43f33839
unknown <deekayedm@gmail.com>
2025-11-10 14:07:22 +0530

    added route

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 
-const  express = require("express");
-const  cors = require("cors");
-const  path = require("path");
-const  connectDB = require('./config/db.js');
-const  authRoutes = require('./routes/authRoutes.js');
-const  userRoutes = require('./routes/userRoutes.js');
-const  taskRoutes = require('./routes/taskRoutes.js');
-const  reportRoutes = require('./routes/reportRoutes.js');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require('./config/db.js');
+const authRoutes = require('./routes/authRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
+const taskRoutes = require('./routes/taskRoutes.js');
+const reportRoutes = require('./routes/reportRoutes.js');
 
 
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || '*',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type' ,'Authorization']
+  origin: process.env.CLIENT_URL || '*',
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 // Middleware
@@ -27,6 +27,9 @@ app.use(express.json());
 
 
 // Routes
+app.use('/api/hi', (req, res) => {
+  res.json({ message: "Hi User" })
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
@@ -36,5 +39,5 @@ app.use("/upload", express.static(path.join(__dirname, "uploads")));
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running ${PORT}`)
+  console.log(`Server is running ${PORT}`)
 })
