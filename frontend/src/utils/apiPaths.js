5cf7c5049bab82a196d7fa02af708546d088decb
unknown <deekayedm@gmail.com>
2025-11-17 19:19:08 +0530

    fixed some issue

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,6 +1,5 @@
 
-export const BASE_URL = "http://localhost:5000";
-
+export const BASE_URL = import.meta.env.VITE_APP_API_URL;
 export const API_PATHS = {
     AUTH: {
         REGISTER: "/api/auth/register",
